41760ad95917faaddfa36e40efd09239133c8a9b
allamurodxakimov <xakimov2806@gmail.com>
2024-01-26 10:53:21 +0300

    deployment

--- app.py
+++ app.py
@@ -3,7 +3,14 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def home_page():
-    return "Assalomu aleykum"
+    return "Bosh sahifaga xush kelibsiz"
+@app.route('/about')
+def about():
+    return "About sahifaga xush kelibsiz"
+
+@app.route('/contact')
+def contact():
+    return "Contact sahifaga xush kelibsiz"
 
 if __name__ == "__main__":
     app.run(debug=True)
